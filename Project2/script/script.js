d0db87cbc68c1b37aa54543f668088fd2beac9c9
j-espada <kim.espada@gmail.com>
2017-01-03 14:53:35 +0100

    Personal website
    Personal website with lighbox, Google maps integrations , forms validations ...

---
+++ Project2/script/script.js
@@ -0,0 +1,37 @@
+function openModal(n) {
+    document.getElementById('myModal').style.display = "block";
+    $(".navbar-form").toggle();
+    currentSlide(n);
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+    $(".navbar-form").toggle();
+}
+
+var slideIndex = 1;
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+
+}
+
